55464ccc4dd9b3eaa5ba92e00757eab7b56400d1
Orsi Suto <orsolya.suto.so@gmail.com>
2023-05-16 12:27:52 +0200

    Auto-generated commit [ci skip]

--- src/app/quiz/helper-text.ts
+++
@@ -1,1 +0,0 @@
-export const helperText = 'Keress meg és írd be a kódot a továbblépéshez';
--- src/app/quiz/welcome-text.ts
+++
@@ -1,5 +0,0 @@
-export const welcomeText = `Szia Edi! Születésnapod alakalmából egy kis játékkal készültünk Neked.
-Az első pár kérdés feleletválasztós, szóval itt még hibázhatsz is. Ám a quiz második felében nagyobb
-a tét: elrejtettem a lakásban a kedvcsináló ajándékod darabjat. 
-Ahhoz, hogy átvehesd a végső ajándékodat, keresd meg ezeket a darabokat és kövesd az utasítást.
-Sok sikert hozzá és jó szórakozást!`;
